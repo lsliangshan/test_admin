7d080c673ad87140c842ecd63e2364f805cf0fea
liangshan <loveliangshan@126.com>
2015-12-02 16:28:55 +0800

    fix bug

--- App/Common/Conf/config.js
+++ App/Common/Conf/config.js
@@ -13,11 +13,11 @@ module.exports = {
     default_group: 'Admin',//默认分组
     /**数据库配置**/
     db_type: 'mysql', // 数据库类型
-    db_host: '10.0.2.40', // 服务器地址
+    db_host: '127.0.0.1', // 服务器地址
     db_port: '3306', // 端口
     db_name: 'test', // 数据库名
     db_user: 'root', // 用户名
-    db_pwd: 'richenlin', // 密码
+    db_pwd: 'root', // 密码
     db_prefix: '', // 数据库表前缀
 
     url_resource_reg: /^(Static\/|favicon\.ico|robot\.txt)/, //判断是否是静态资源的正则
@@ -27,11 +27,4 @@ module.exports = {
     post_max_file_size: 300 * 1024 * 1024, //上传文件大小限制，默认300M
     post_file_upload_type:'aliyun',//上传文件方式: local,ftp,aliyun,aws
 
-    /**Aliyun config**/
-    ali_access_key_id: 'gBvfSrZjoFhRCs08',
-    ali_access_key_secret: 'b6VEEubulOXPbRUj2L2J9IGb88UYds',
-    ali_bucket: 'cnautoshows',
-    ali_region: 'oss-cn-hangzhou',
-    ali_path: '',
-    ali_url: 'http://img.cnautoshows.com/',
 };
