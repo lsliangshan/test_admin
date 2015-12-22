a415a6e3a1859344af6e39bdf6ccf09097a47d72
liangshan <loveliangshan@126.com>
2015-12-22 23:09:47 +0800

    fix bug

--- App/Common/Conf/config.js
+++ App/Common/Conf/config.js
@@ -11,16 +11,10 @@ module.exports = {
     app_port: 3000,
     app_group_list: ['Admin'], //分组列表
     default_group: 'Admin',//默认分组
-    /**数据库配置**/
-    db_type: 'mysql', // 数据库类型
-    db_host: '182.92.228.74', // 服务器地址
-    db_port: '3306', // 端口
-    db_name: 'test', // 数据库名
-    db_user: 'root', // 用户名
-    db_pwd: '*#liangshan*#', // 密码
-    db_prefix: '', // 数据库表前缀
+
 
     url_resource_reg: /^(Static\/|favicon\.ico|robot\.txt)/, //判断是否是静态资源的正则
 
+    post_file_allow_type: 'jpg|jpeg|png|bmp|gif|xls|xlsx|zip|rar|ipa|apk',//允许上传的文件类型
 
 };
